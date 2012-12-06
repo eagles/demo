c0e38b7fc6738c3255f5619caaf59c35c261f06f
Eason Wu <easonwu85@gmail.com>
2012-12-06 21:07:40 +0800

    #7 1. Update the gitignore configuration file to add Java project ignore settings; 2. Add a project JacksonDemo to demonstrate how to handle JSON object with Jackson package.

---
+++ JacksonDemo/src/fjxy/demo/jackson/Bean2Json.java
@@ -0,0 +1,59 @@
+package fjxy.demo.jackson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Bean2Json {
+
+    public ExtJsTreeNode init() {
+        ExtJsTreeNode tree = new ExtJsTreeNode();
+        
+        tree.setId("Root");
+        tree.setText("Root");
+        tree.setLeaf(false);
+        // Build Children
+        List<ExtJsTreeNode> children = buildChildren();
+        tree.setChildren(children);
+        
+        return tree;
+    }
+
+    private List<ExtJsTreeNode> buildChildren() {
+        List<ExtJsTreeNode> nodes = new ArrayList<ExtJsTreeNode>();
+        
+        for (int i = 0; i < 10; i++) {
+            ExtJsTreeNode node = new ExtJsTreeNode();
+            node.setId("Node" + i);
+            node.setText("Node" + i);
+            node.setLeaf(true);
+            nodes.add(node);
+        }
+        
+        return nodes;
+    }
+    
+    public void convertToJson(ExtJsTreeNode tree) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.setSerializationInclusion(Include.NON_NULL); // Don't output null fields
+        String json = mapper.writeValueAsString(tree);
+
+        System.out.println(json);
+    }
+
+    /**
+     * @param args
+     * @throws JsonProcessingException 
+     */
+    public static void main(String[] args) throws JsonProcessingException {
+        Bean2Json jsonUtil = new Bean2Json();
+        ExtJsTreeNode tree = jsonUtil.init();
+        jsonUtil.convertToJson(tree);
+
+    }
+
+}
---
+++ JacksonDemo/src/fjxy/demo/jackson/ExtJsTreeNode.java
@@ -0,0 +1,86 @@
+package fjxy.demo.jackson;
+
+import java.util.List;
+
+public class ExtJsTreeNode {
+
+    private String id;
+    private String text;
+    private Boolean leaf;
+    private Boolean expanded;
+    private List <ExtJsTreeNode> children;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    /**
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
+
+    /**
+     * @param text the text to set
+     */
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    /**
+     * @return the leaf
+     */
+    public Boolean getLeaf() {
+        return leaf;
+    }
+
+    /**
+     * @param leaf the leaf to set
+     */
+    public void setLeaf(Boolean leaf) {
+        this.leaf = leaf;
+    }
+
+    /**
+     * @return the expanded
+     */
+    public Boolean getExpanded() {
+        this.expanded = this.leaf ? false : true;
+        return this.expanded;
+    }
+
+    /**
+     * @return the children
+     */
+    public List <ExtJsTreeNode> getChildren() {
+        return children;
+    }
+
+    /**
+     * @param children the children to set
+     */
+    public void setChildren(List <ExtJsTreeNode> children) {
+        this.children = children;
+    }
+    
+    public ExtJsTreeNode() {}
+
+    public ExtJsTreeNode(String id, String text, Boolean leaf, Boolean expanded, List<ExtJsTreeNode> children) {
+        this.id = id;
+        this.text = text;
+        this.leaf = leaf;
+        this.expanded = expanded;
+        this.children = children;
+    }
+}
